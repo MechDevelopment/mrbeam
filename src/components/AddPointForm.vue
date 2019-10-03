a35615d11747b69d112114f6867e4f3c4bec9c0a
vpvpvpvp <vktrpnzrv@gmail.com>
2019-10-04 01:52:13 +0300

    Form Styling

--- src/components/AddPointForm.vue
+++ src/components/AddPointForm.vue
@@ -1,35 +1,40 @@
 <template>
   <form>
-    <b-field>
+    <!-- TAB BUTTONS -->
+    <b-field position="is-centered">
       <b-radio-button v-model="radioButton" native-value="load" type="is-danger">
-        <b-icon icon="close"></b-icon>
+        <b-icon pack="fas" icon="arrow-down"></b-icon>
         <span>Load</span>
       </b-radio-button>
 
       <b-radio-button v-model="radioButton" native-value="defload" type="is-success">
-        <b-icon icon="check"></b-icon>
+        <b-icon pack="fas" icon="angle-double-down"></b-icon>
         <span>Def. Load</span>
       </b-radio-button>
 
-      <b-radio-button v-model="radioButton" native-value="momentum">Mom</b-radio-button>
+      <b-radio-button v-model="radioButton" native-value="momentum" type="is-warning">
+        <b-icon pack="fas" icon="blind"></b-icon>
+        <span>Mom</span>
+      </b-radio-button>
 
-      <b-radio-button v-model="radioButton" native-value="defenition">Defenition</b-radio-button>
+      <b-radio-button v-model="radioButton" native-value="defenition">
+        <b-icon pack="fas" icon="align-center"></b-icon>
+        <span>Def</span>
+      </b-radio-button>
     </b-field>
 
-    <b-field v-show="radioButton == 'defenition'">
-      <b-radio-button v-model="defenitionType" native-value="1" type="is-danger">
-        <b-icon icon="close"></b-icon>
+    <b-field v-show="radioButton == 'defenition'" position="is-centered">
+      <b-radio v-model="defenitionType" native-value="1" type="is-danger">
         <span>Load</span>
-      </b-radio-button>
+      </b-radio>
 
-      <b-radio-button v-model="defenitionType" native-value="2" type="is-success">
-        <b-icon icon="check"></b-icon>
+      <b-radio v-model="defenitionType" native-value="2" type="is-success">
         <span>Def. Load</span>
-      </b-radio-button>
+      </b-radio>
     </b-field>
 
     <b-field>
-      <b-input placeholder="X-coordinate" type="number"></b-input>
+      <b-input placeholder="X-coordinate" type="number" expanded></b-input>
       <b-select placeholder="Meters">
         <option>Sm</option>
         <option>Mm</option>
@@ -37,7 +42,7 @@
     </b-field>
 
     <b-field v-show="radioButton == 'defload'">
-      <b-input placeholder="Y-coordinate" type="number"></b-input>
+      <b-input placeholder="Y-coordinate" type="number" expanded></b-input>
       <b-select placeholder="Meters">
         <option>Sm</option>
         <option>Mm</option>
@@ -45,11 +50,11 @@
     </b-field>
 
     <b-field v-show="radioButton == 'load'">
-      <b-input placeholder="Angle" type="number"></b-input>
+      <b-input placeholder="Angle" type="number" expanded></b-input>
     </b-field>
 
     <b-field v-show="radioButton != 'defenition'">
-      <b-input placeholder="Load" type="number"></b-input>
+      <b-input placeholder="Load" type="number" expanded></b-input>
       <b-select placeholder="N/m">
         <option>N/sm</option>
         <option>kN/m</option>
@@ -69,14 +74,7 @@
 </template>
 
 <script>
-import LoadForm from "./LoadForm.vue";
-import DistLoadForm from "./DistLoadForm.vue";
-
 export default {
-  components: {
-    LoadForm,
-    DistLoadForm
-  },
   data() {
     return {
       radioButton: "load",
