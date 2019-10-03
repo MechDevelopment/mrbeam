33b34c8a3a197de77626990b2a7ad421ef67b795
vpvpvpvp <vktrpnzrv@gmail.com>
2019-10-03 21:18:57 +0300

    Styles for inputs

--- src/components/LoadForm.vue
+++ src/components/LoadForm.vue
@@ -1,28 +1,19 @@
 <template>
   <form>
-    <b-field>
-      <b-select placeholder="Select a type">
-        <option value="1">Bulma</option>
-        <option value="2">Vue.js</option>
-        <option value="3">Buefy</option>
-      </b-select>
-    </b-field>
-
-    <b-field>
-      <b-radio native-value="on-border">On Border</b-radio>
-      <b-radio native-value="inside">Inside</b-radio>
-    </b-field>
-
-    <b-field>
-      <b-input placeholder="X-coordinate" type="number"></b-input>
+    <b-field label="X-coordinate" :label-position="labelPosition">
+      <b-input type="number"></b-input>
       <b-select placeholder="Meters">
         <option>Sm</option>
         <option>Mm</option>
       </b-select>
     </b-field>
 
-    <b-field>
-      <b-input placeholder="Load" type="number"></b-input>
+    <b-field label="Angle" :label-position="labelPosition">
+      <b-input type="number"></b-input>
+    </b-field>
+
+    <b-field label="Load" :label-position="labelPosition">
+      <b-input type="number"></b-input>
       <b-select placeholder="N/m">
         <option>N/sm</option>
         <option>kN/m</option>
@@ -33,6 +24,11 @@
 
 <script>
 export default {
-  name: "LoadForm"
+  name: "LoadForm",
+  data() {
+    return {
+      labelPosition: "on-border"
+    };
+  }
 };
 </script>
