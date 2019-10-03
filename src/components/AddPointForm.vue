de78a898d1791316d21be465cf6ef4799b7ef0e3
vpvpvpvp <vktrpnzrv@gmail.com>
2019-10-04 00:51:21 +0300

    Refactored tabs. Tabs are based on checkboxes. There is only one form now.

--- src/components/AddPointForm.vue
+++ src/components/AddPointForm.vue
@@ -1,25 +1,21 @@
 <template>
-  <section>
-    <div class="buttons">
-      <b-button v-for="tab in tabs" v-bind:key="tab" v-on:click="currentTab = tab">{{ tab }}</b-button>
-      <!-- <b-button class="button" type="is-primary submit" icon-pack="fas" icon-left="arrow-down">Load</b-button>
-      <b-button
-        class="button"
-        type="is-primary submit"
-        icon-pack="fas"
-        icon-left="angle-double-down"
-      >DistLoad</b-button>
-      <b-button
-        class="button"
-        type="is-primary submit"
-        icon-pack="fas"
-        icon-left="align-center"
-      >Definition</b-button>
-      <b-button class="button" type="is-primary" icon-pack="fas" icon-left="blind">Momentum</b-button>-->
-    </div>
+  <form>
+    <b-field>
+      <b-radio-button v-model="radioButton" native-value="load" type="is-danger">
+        <b-icon icon="close"></b-icon>
+        <span>Load</span>
+      </b-radio-button>
 
-    <!-- <load-form/>
-    <dist-load-form/>-->
+      <b-radio-button v-model="radioButton" native-value="defload" type="is-success">
+        <b-icon icon="check"></b-icon>
+        <span>Def. Load</span>
+      </b-radio-button>
+
+      <b-radio-button v-model="radioButton" native-value="momentum">Mom</b-radio-button>
+
+      <b-radio-button v-model="radioButton" native-value="defenition">Defenition</b-radio-button>
+    </b-field>
+
     <keep-alive>
       <component v-bind:is="currentTabComponent"></component>
     </keep-alive>
@@ -33,7 +29,7 @@
       >Add Point</b-button>
       <b-button icon-pack="fas" icon-right="calculator" type="is-light">Analyse Beam</b-button>
     </div>
-  </section>
+  </form>
 </template>
 
 <script>
@@ -48,7 +44,8 @@ export default {
   data() {
     return {
       currentTab: "Load",
-      tabs: ["Load", "DistLoad", "Momentum", "Defenition"]
+      tabs: ["Load", "DistLoad", "Momentum", "Defenition"],
+      radioButton: "Yep"
     };
   },
   computed: {
