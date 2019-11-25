37363b0b562fdcc154e050b56f94390793dc44af
vpvpvpvp <vktrpnzrv@gmail.com>
2019-11-25 21:14:35 +0300

    test page

--- src/views/NewBeam2.vue
+++ src/views/NewBeam2.vue
@@ -1,17 +1,21 @@
 <template>
   <section class="section has-background-white-ter">
     <div class="container">
-      <div class="column">
-        <result-view />
-      </div>
-      <div class="column">
-        <div class="columns">
-          <div class="column">
-            <add-point-form />
-          </div>
-          <div class="column">
-            <table-points />
-          </div>
+      <div class="columns is-desktop">
+        <!-- <div class="column is-two-thirds"> -->
+        <div class="column">
+          <!-- <div class="card">
+            <img
+              src="https://img05.rl0.ru/afisha/-x700/s5.afisha.net/MediaStorage/b7/8d/d78fe037939e47a9909975a18db7.jpg"
+              alt
+            />
+          </div>-->
+          <result-view />
+          <result-view />
+        </div>
+        <div class="column">
+          <add-point-form />
+          <table-points />
         </div>
       </div>
     </div>
