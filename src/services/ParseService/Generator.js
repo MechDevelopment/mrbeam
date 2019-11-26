7fe17bf45687b8878dcd582aca3d0f77af7df33f
divasilevski <divasilevski@gmail.com>
2019-11-26 23:10:29 +0300

    Prepearing to the new JSON points

--- src/services/ParseService/Generator.js
+++ src/services/ParseService/Generator.js
@@ -3,5 +3,16 @@ class Generator {
      * @param {Number} count count of point
      * @param {Number} complexity beam complexity (from 0 to 2)
     */
-    constructor(count, complexity = "random") {}
+    constructor(count, complexity = "random") {
+        /*
+        
+        complexity 0 - Симметричные балки с распределнной нагрузкой,
+        моментами или силами, на выходе будем иметь только эпюры и реакции.
+        
+        complexity 1 - Убираем симметрию, добавляем шарниры и единый материал. 
+        
+        complexity 2 - Добавляем различный материал.
+        
+        */
+    }
 }
