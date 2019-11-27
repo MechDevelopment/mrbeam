0482001b294cc8e9a6d052ccf19651c024728103
divasilevski <divasilevski@gmail.com>
2019-11-27 18:27:46 +0300

    Created Generator

--- src/services/ParseService/Generator.js
+++ src/services/ParseService/Generator.js
@@ -1,4 +1,5 @@
-import {sortX} from "./Parser.js"
+import { sortX } from "./Parser";
+import { randint } from "../Utilus";
 
 class Generator {
     /** Create JSON points 
@@ -16,5 +17,86 @@ class Generator {
         complexity 2 - Добавляем различный материал.
         
         */
+
+        let objects = [];
+        const N = count - 1;
+        let shift = null;
+        let type;
+
+        if (complexity == "random") {
+            complexity = 1;
+        }
+
+        if (complexity >= 1) {
+            // Первым делом сгенерируем закрепление(ия)
+            if (randint(0, 1)) {
+                // Жесткое
+                if (randint(0, 1)) {
+                    // В начале отрезка
+                    objects.push(create(0, [0], 3, [3]));
+                } else {
+                    // В конце отрезка
+                    objects.push(create(N, [N], 3, [3]));
+                }
+            } else {
+                // Шарнирное
+                shift = randint(0, ((N - 1) / 2) >> 0);
+                objects.push(create(0 + shift, [0 + shift], 3, [2]));
+                objects.push(create(N - shift, [N - shift], 3, [1]));
+            }
+
+            // Сгенерируем тип нагрузки
+            if (randint(0, 1)) {
+                type = 1;
+                for (let id = 0; id < count; id++) {
+                    // Не стоит создавать лишние точки
+                    if (shift != null) {
+                        if (objects[0].id != id && objects[1].id != id) {
+                            objects.push(
+                                create(id, [id], type, [randint(-3, 3) * 50, 90])
+                            );
+                        }
+                    } else {
+                        if (objects[0].id != id) {
+                            objects.push(
+                                create(id, [id], type, [randint(-3, 3) * 50, 90])
+                            );
+                        }
+                    }
+                }
+            } else {
+                type = 2;
+                for (let id = 0; id < count; id++) {
+                    // Не стоит создавать лишние точки
+                    if (shift != null) {
+                        if (objects[0].id != id && objects[1].id != id) {
+                            objects.push(
+                                create(id, [id], type, [randint(-3, 3) * 50])
+                            );
+                        }
+                    } else {
+                        if (objects[0].id != id) {
+                            objects.push(
+                                create(id, [id], type, [randint(-3, 3) * 50])
+                            );
+                        }
+                    }
+                }
+            }
+
+            
+        }
+        sortX(objects);
+        this.results = objects;
+    }
+
+    getResults() {
+        return this.results;
     }
 }
+
+function create(id, x, type, value) {
+    return { id, x, type, value };
+}
+
+export default Generator;
