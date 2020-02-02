d34ad4dc46d017e24aef392c9af71658a70a9bfb
divasilevski <divasilevski@gmail.com>
2020-02-02 13:14:35 +0300

    moment and slope added

--- src/services/FemService/FCharts.js
+++ src/services/FemService/FCharts.js
@@ -3,19 +3,33 @@ function chartResults(elems) {
   let labels = [];
   let displacement = [];
   let shear = [];
+  let moment = [];
+  let slopeRadians = [];
+  let slopeDegrees = [];
 
   // Create charts
   for (let i in elems) {
     labels.push(elems[i].nodes[0].x, elems[i].nodes[1].x);
-    displacement.push(elems[i].solution[0], elems[i].solution[2]);
     shear.push(elems[i].reaction[0], -elems[i].reaction[2]);
+    moment.push(elems[i].reaction[1], -elems[i].reaction[3]);
+
+    displacement.push(elems[i].solution[0], elems[i].solution[2]);
+    slopeRadians.push(elems[i].solution[1], elems[i].solution[3]);
+    slopeDegrees.push(
+      (elems[i].solution[1] * 180) / Math.PI,
+      (elems[i].solution[3] * 180) / Math.PI
+    );
   }
 
   console.log(elems);
   return {
     labels,
+    shear,
+    moment,
     displacement,
-    shear
+    slopeRadians,
+    slopeDegrees
+
     // max_displacement: Math.max(..._disp),
     // min_displacement: Math.min(..._disp),
     // shear: _shear,
