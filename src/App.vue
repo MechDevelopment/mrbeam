6c3ea1cf9e62ebc7f4ebd74519ac988a42595d8b
divasilevski <divasilevski@gmail.com>
2020-04-09 15:24:13 +0300

    cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <p>MrBEAM v 0.2.0</p>
+    <div class="center first">Дезигнед</div>
+    <ul>
+      <li>Карусель</li>
+      <li>Настройки темы и языка</li>
+      <li>Кеширование</li>
+      <li>Набор данных</li>
+      <li>Таблица для данных</li>
+      <li>Вывод графиков и таблиц</li>
+      <li>Визуальное построение</li>
+      <li>Инструкция</li>
+    </ul>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+p
+  font-size: 2em
+  text-transform: uppercase
+  text-decoration: underline overline
+  letter-spacing: 0.1em
+  text-shadow:  1px 1px 3px blue, 2px 2px 3px red, 3px 3px 3px orange
+  text-align: center
+  margin-top: 200px
 
-#nav {
-  padding: 30px;
+.center
+  text-align: center
+  font-weight: 700
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.first::first-letter
+  color: red
+  background: blue
+  font-size: 3em
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+li
+  list-style-type: square
+
+li:nth-of-type(2n)
+  list-style-type: circle
+
 </style>
