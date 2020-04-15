fabdedf9c7f2c53d2d51bb058773ce053b195680
divasilevski <divasilevski@gmail.com>
2020-04-15 17:18:11 +0300

    renaming and build function

--- src/components/layout/layout.component.vue
+++ src/components/layout/layout.component.vue
@@ -10,17 +10,13 @@
       :key="index + 'component'"
       name="fade"
       :enter-active-class="
-        anim == 'left' ? 'animated slideInLeft' : 'animated slideInRight'
+        direction == 'left' ? 'animated slideInLeft' : 'animated slideInRight'
       "
       :leave-active-class="
-        anim == 'left' ? 'animated slideOutRight' : 'animated slideOutLeft'
+        direction == 'left' ? 'animated slideOutRight' : 'animated slideOutLeft'
       "
     >
-      <div
-        v-show="show[index]"
-        class="toggle"
-        :style="toggle_style[index] + t_s[index]"
-      >
+      <div v-show="show_slots[index]" class="wrap" :style="wrap_style[index]">
         <div class="item main">
           <Render :vnode="component"></Render>
         </div>
@@ -29,7 +25,7 @@
 
     <div class="dots">
       <button @click="left">LEFT</button>
-      {{ show }}
+      {{ show_slots }}
       <button @click="right">RIGHT</button>
     </div>
   </div>
@@ -38,34 +34,63 @@
 <script>
 export default {
   data: () => ({
-    components: ["Info", "Input", "Chart", "Data"],
-    n: 4,
-    m: undefined,
-    show: [0, 1, 1, 0],
-    anim: "left",
-    toggle_style: ["width: 46%", "width: 46%", "width: 46%", "width: 46%"],
-    t_s: ["", "left: 4%", "left: 50%", "left: 50%"],
+    count_of_slots: undefined,
+    first_active_slot: 0,
+
+    show_slots: undefined,
+    wrap_style: undefined,
+
+    direction: "left",
+    
     commands: [],
     timer: undefined,
     duration: 1000,
   }),
 
   created() {
-
     this.onResize();
   },
 
-  mounted() {
-    window.addEventListener("resize", this.onResize);
-  },
-
   methods: {
+    buildData(n, m) {
+      // build show_slots
+      this.show_slots = [];
+      for (let i = 0; i < n; i++) {
+        if (i < m) this.show_slots.push(1);
+        else this.show_slots.push(0);
+      }
+
+      // build wrap_style
+      this.wrap_style = [];
+      let j = 0;
+      let left = "0%";
+      for (let i = 0; i < n; i++) {
+        if (this.show_slots[i] == 1 || j == m) {
+          if (j == m) left = `${(100 / m) * (j - 1)}%`;
+          else left = `${(100 / m) * j}%`;
+          j++;
+        } else {
+          left = "0%";
+        }
+        this.wrap_style.push({
+          width: `${100 / m}%`,
+          left: left,
+        });
+      }
+
+      console.log(this.show_slots, this.wrap_style);
+    },
+
     onResize() {
       if (window.innerWidth > 800) {
-        this.m = 2;
+        this.count_of_slots = 2;
       } else {
-        this.m = 1;
+        this.count_of_slots = 1;
       }
+
+      console.log("n, m", this.$slots.default.length, this.count_of_slots);
+
+      this.buildData(this.$slots.default.length, this.count_of_slots);
     },
 
     left() {
@@ -97,28 +122,36 @@ export default {
     },
 
     l() {
-      this.anim = "left";
-      this.show.push(this.show.shift());
-      let s = this.show;
-      this.show = [0, 0, 0, 0];
+      this.direction = "left";
+      this.show_slots.push(this.show_slots.shift());
+      let s = this.show_slots;
+      this.show_slots = [0, 0, 0, 0];
 
       setTimeout(() => {
-        this.t_s.push(this.t_s.shift());
-        this.show = s;
+        this.wrap_style.push(this.wrap_style.shift());
+        this.show_slots = s;
       });
+
+      console.log(this.wrap_style);
     },
 
     r() {
-      this.anim = "right";
-      this.show.unshift(this.show.pop());
-      let s = this.show;
-      this.show = [0, 0, 0, 0];
+      this.direction = "right";
+      this.show_slots.unshift(this.show_slots.pop());
+      let s = this.show_slots;
+      this.show_slots = [0, 0, 0, 0];
 
       setTimeout(() => {
-        this.t_s.unshift(this.t_s.pop());
-        this.show = s;
+        this.wrap_style.unshift(this.wrap_style.pop());
+        this.show_slots = s;
       });
+
+      console.log(this.wrap_style);
     },
+  },
+
+  mounted() {
+    window.addEventListener("resize", this.onResize);
   },
 
   beforeDestroy() {
@@ -129,21 +162,21 @@ export default {
     m(key) {
       setTimeout(() => {
         if (key == 1) {
-          this.toggle_style = ["", "", "", ""];
+          this.wrap_style = ["", "", "", ""];
         } else {
-          for (let i = 0; i < this.n; i++) {
-            this.toggle_style[i] = `width: ${100 / this.m - 4}%;`;
+          for (let i = 0; i < this.$slots.default.length; i++) {
+            this.wrap_style[i] = `width: ${100 / this.count_of_elems - 4}%;`;
           }
         }
-        console.log(this.toggle_style);
       });
     },
   },
 
   components: {
+    // Component for rendering $slots items
     Render: {
       functional: true,
-      render: (h, ctx) => ctx.props.vnode
+      render: (h, ctx) => ctx.props.vnode,
     },
   },
 };
