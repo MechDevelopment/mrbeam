d6bad97f827fa7661c08446986efffee13d3e1f4
divasilevski <divasilevski@gmail.com>
2020-04-30 23:10:30 +0300

    form inputs

--- src/components/input/form/form.component.vue
+++ src/components/input/form/form.component.vue
@@ -7,10 +7,14 @@
       <button class="main" @click="clickDefenition">Def</button>
       <button class="main" @click="clickMaterial">Mat</button>
     </span>
-    <div></div>
 
     <div></div>
     <span>{{ unitNumber }}</span>
+    
+    <div v-for="(form, index) in unitInput" :key="index + 'unitInput'">
+      <span v-if="form.type == 'input'">{{form.label}} <input /></span>
+    </div>
+
   </div>
 </template>
 
@@ -18,23 +22,47 @@
 export default {
   data: () => ({
     unitNumber: 0,
+    unitInput: [
+      { type: "input", label: "x" },
+      { type: "input", label: "y" },
+    ],
   }),
 
   methods: {
     clickLoad() {
-      this.unitNumber = 0
+      this.unitNumber = 0;
+      this.unitInput = [
+        { type: "input", label: "x" },
+        { type: "input", label: "P" },
+      ];
     },
     clickMoment() {
-      this.unitNumber = 1
+      this.unitNumber = 1;
+      this.unitInput = [
+        { type: "input", label: "x" },
+        { type: "input", label: "M" },
+      ];
     },
     clickDistload() {
-      this.unitNumber = 2
+      this.unitNumber = 2;
+      this.unitInput = [
+        { type: "input", label: "x1" },
+        { type: "input", label: "P1" },
+        { type: "input", label: "x2" },
+        { type: "input", label: "P2" },
+      ];
     },
     clickDefenition() {
-      this.unitNumber = 3
+      this.unitNumber = 3;
+      this.unitInput = [{ type: "input", label: "x" }];
     },
     clickMaterial() {
-      this.unitNumber = 4
+      this.unitNumber = 4;
+      this.unitInput = [
+        { type: "input", label: "E" },
+        { type: "input", label: "J" },
+        { type: "input", label: "A" }
+      ];
     },
   },
 };
