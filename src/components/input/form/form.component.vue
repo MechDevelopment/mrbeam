eece682d3c8cc5d6145ae49a37df3271b79a9564
divasilevski <divasilevski@gmail.com>
2020-04-30 18:31:52 +0300

    toolset scss

--- src/components/input/form/form.component.vue
+++ src/components/input/form/form.component.vue
@@ -1,26 +1,41 @@
 <template>
   <div class="form">
-      <span class="toolset">
-        <button>Load</button>
-        <button>Moment</button>
-        <button>Dist</button>
-        <button>Def</button>
-        <button>Mat</button>
-      </span>
-      <div></div>
-      <span class="toolset">
-        <button>x</button>
-        <input />
-        <div></div>
-        <button>p</button>
-        <input />
-      </span>
-    </div>
+    <span class="toolset">
+      <button class="main" @click="clickLoad">Load</button>
+      <button class="main" @click="clickMoment">Moment</button>
+      <button class="main" @click="clickDistload">Dist</button>
+      <button class="main" @click="clickDefenition">Def</button>
+      <button class="main" @click="clickMaterial">Mat</button>
+    </span>
+    <div></div>
+
+    <div></div>
+    <span>{{ unitNumber }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-  mounted() {
+  data: () => ({
+    unitNumber: 0,
+  }),
+
+  methods: {
+    clickLoad() {
+      this.unitNumber = 0
+    },
+    clickMoment() {
+      this.unitNumber = 1
+    },
+    clickDistload() {
+      this.unitNumber = 2
+    },
+    clickDefenition() {
+      this.unitNumber = 3
+    },
+    clickMaterial() {
+      this.unitNumber = 4
+    },
   },
 };
 </script>
