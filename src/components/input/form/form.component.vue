f01fc19c7a41463b60691c447613d289885d010e
divasilevski <divasilevski@gmail.com>
2020-04-30 17:37:59 +0300

    create form component

---
+++ src/components/input/form/form.component.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form">
+      <span class="toolset">
+        <button>Load</button>
+        <button>Moment</button>
+        <button>Dist</button>
+        <button>Def</button>
+        <button>Mat</button>
+      </span>
+      <div></div>
+      <span class="toolset">
+        <button>x</button>
+        <input />
+        <div></div>
+        <button>p</button>
+        <input />
+      </span>
+    </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+@import "../../../styles.scss"
+@import "./form.style.scss"
+</style>
