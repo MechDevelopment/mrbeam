369ae835785dd8583e1a31ec9fc8ae0080157fb0
divasilevski <divasilevski@gmail.com>
2020-05-01 18:53:53 +0300

    new folder for icons & themify them on  input form

--- src/components/input/form/form.component.vue
+++ src/components/input/form/form.component.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="form">
     <span class="toolset">
-      <button class="main" @click="clickLoad"><div class="svg-icon"></div></button>
-      <button class="main" @click="clickMoment"><div class="svg-icon"></div></button>
-      <button class="main" @click="clickDistload"><div class="svg-icon"></div></button>
-      <button class="main" @click="clickDefenition"><div class="svg-icon"></div></button>
-      <button class="main" @click="clickMaterial"><div class="svg-icon"></div></button>
+      <div :class="unitNumber == 0 ? 'main-invert' : 'main'" @click="clickLoad">
+        <div
+          class="icon-load svg-icon"
+          :class="unitNumber == 0 ? 'main' : 'main-invert'"
+        ></div>
+      </div>
+      <div
+        :class="unitNumber == 1 ? 'main-invert' : 'main'"
+        @click="clickMoment"
+      >
+        <div
+          class="icon-load svg-icon"
+          :class="unitNumber == 1 ? 'main' : 'main-invert'"
+        ></div>
+      </div>
+      <div
+        :class="unitNumber == 2 ? 'main-invert' : 'main'"
+        @click="clickDistload"
+      >
+        <div
+          class="icon-load svg-icon"
+          :class="unitNumber == 2 ? 'main' : 'main-invert'"
+        ></div>
+      </div>
+      <div
+        :class="unitNumber == 3 ? 'main-invert' : 'main'"
+        @click="clickDefenition"
+      >
+        <div
+          class="icon-load svg-icon"
+          :class="unitNumber == 3 ? 'main' : 'main-invert'"
+        ></div>
+      </div>
+      <div
+        :class="unitNumber == 4 ? 'main-invert' : 'main'"
+        @click="clickMaterial"
+      >
+        <div
+          class="icon-load svg-icon"
+          :class="unitNumber == 4 ? 'main' : 'main-invert'"
+        ></div>
+      </div>
     </span>
 
     <div></div>
     <span>{{ unitNumber }}</span>
-    
+
     <div v-for="(form, index) in unitInput" :key="index + 'unitInput'">
-      <span v-if="form.type == 'input'">{{form.label}} <input /></span>
+      <span v-if="form.type == 'input'">{{ form.label }} <input /></span>
     </div>
 
     <button>ADD UNIT</button>
@@ -20,7 +57,6 @@
 </template>
 
 <script>
-
 export default {
   data: () => ({
     unitNumber: 0,
@@ -63,10 +99,10 @@ export default {
       this.unitInput = [
         { type: "input", label: "E" },
         { type: "input", label: "J" },
-        { type: "input", label: "A" }
+        { type: "input", label: "A" },
       ];
     },
-  }
+  },
 };
 </script>
 
