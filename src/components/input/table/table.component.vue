5bfa575b8b6f6727a300e06ab7df3de6a601e52d
divasilevski <divasilevski@gmail.com>
2020-05-29 17:25:06 +0300

    added table component

--- src/components/input/table/table.component.vue
+++ src/components/input/table/table.component.vue
@@ -1,3 +1,16 @@
 <template>
-    <div></div>
+  <div class="table-form">Table</div>
 </template>
+
+<script>
+export default {
+  data: () => ({}),
+  mounted() {
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+@import "../../../styles.scss"
+@import "./table.style.scss"
+</style>
