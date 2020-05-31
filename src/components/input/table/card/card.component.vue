46196a6cf760a66cff8fe1849e940ac8742a4ad7
divasilevski <divasilevski@gmail.com>
2020-05-31 15:56:02 +0300

    card component

---
+++ src/components/input/table/card/card.component.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="card">
+    <span class="card-icon main-invert" :class="`icon-${type}`"></span>
+    <span class="card-values math">{{ cardValues(values) }}</span>
+    <div class="card-delete" @click="clickDelete">
+      <span
+        class="iconify"
+        data-icon="eva:close-circle-fill"
+        data-inline="false"
+        data-width="25"
+        data-height="25"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card_id", "type", "values"],
+  methods: {
+    cardValues(v){
+      switch (this.type) {
+        case "load":
+          return `x: ${v.X}     P: ${v.P}`;
+          break;
+        case "moment":
+          return `x: ${v.X}     M: ${v.M}`;
+          break;
+        case "distload":
+          return `x: ${v.X}     x: ${v.X0}     q: ${v.Q}`;
+          break;
+        case "defenition":
+          return `x: ${v.X}`;
+          break;
+        case "material":
+          return `E: ${v.E}     J: ${v.J}     A: ${v.A}`;
+          break;
+        default:
+          return ""
+          break;
+      }
+    },
+    clickDelete() {
+      console.log("card", this.card_id, "has been deleted");
+      this.$el.parentNode.removeChild(this.$el);
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+@import "../../../../styles.scss"
+@import "./card.style.scss"
+</style>
--- src/components/input/table/table.component.vue
+++ src/components/input/table/table.component.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="table-form">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
+    
+    <Card card_id="1" type="load" :values="{X: 12, P: 32}" ></Card>
+    <Card card_id="2" type="moment" :values="{X: 12, M: 32}"></Card>
+    <Card card_id="3" type="distload" :values="{X: 121, X0: 312, Q: 102}"></Card>
+    <Card card_id="4" type="defenition" :values="{X: 12}"></Card>
+    <Card card_id="5" type="material" :values="{E: 12343242, J: 32324324, A: 4343}"></Card>
+
     <span>
       <div class="button but-success">
         <span
@@ -13,7 +16,7 @@
           data-width="20"
           data-height="20"
         ></span>
-        {{"L_Download" | localize}}
+        {{ "L_Download" | localize }}
       </div>
       <div class="button but-success">
         <span
@@ -22,17 +25,24 @@
           data-inline="false"
           data-width="20"
           data-height="20"
-        ></span
-        > {{"L_Clear" | localize}}
+        ></span>
+        {{ "L_Clear" | localize }}
       </div>
     </span>
   </div>
 </template>
 
 <script>
+import Card from "./card/card.component";
+
 export default {
-  data: () => ({}),
+  data: () => ({
+    load_value: {X: 12, P: 32}
+  }),
   mounted() {},
+  components: {
+    Card,
+  },
 };
 </script>
 
