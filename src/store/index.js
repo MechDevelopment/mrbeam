453f9617d393c60d4ffdcca169d0657be548c23c
divasilevski <divasilevski@gmail.com>
2020-06-02 15:11:35 +0300

    added elements array to store

--- src/store/index.js
+++ src/store/index.js
@@ -6,13 +6,17 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     locale: "ru-RU",
+    elements: [],
   },
   mutations: {
     setLocale: (state, locale) => (state.locale = locale),
+    addElement: (state, element) => (state.elements.push(element)),
+    //deleteElementById: (state, id) => (state.elements = ),
   },
   actions: {},
   modules: {},
   getters: {
     locale: (state) => state.locale,
+    elements: (state) => state.elements,
   },
 });
