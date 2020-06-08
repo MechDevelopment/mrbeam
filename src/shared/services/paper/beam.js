13b38b80f068bbea5b34747e5f0f7850266729b9
divasilevski <divasilevski@gmail.com>
2020-06-08 17:37:36 +0300

    paper | draw all points

--- src/shared/services/paper/beam.js
+++ src/shared/services/paper/beam.js
@@ -1,12 +1,13 @@
 const HEIGHT = 75;
-const PADDING = 30;
+const PADDING = 50;
 
 function createBeam(elements, canvasWidth) {
   const beamSize = getBeamSize(elements);
-  console.log(beamSize)
+  console.log(beamSize);
+  beamSize.points.forEach(el => paperPoint(getX(el, beamSize, canvasWidth)))
   console.log(getX(1, beamSize, canvasWidth), canvasWidth);
 
-  paperPoint(getX(beamSize.center, beamSize, canvasWidth))
+  paperPoint();
 
   let rect = new Path.Rectangle(0, 0, canvasWidth, HEIGHT * 2);
   rect.strokeColor = "black";
@@ -17,10 +18,10 @@ function getBeamSize(elements) {
   const xArray = elements.map((el) => el.x).flat();
   let [x, y] = [Math.min(...xArray), Math.max(...xArray)];
   let [length, center] = [y - x, (y + x) / 2];
-  return { x, y, length, center };
+  return { x, y, length, center, points: Array.from(new Set(xArray)) };
 }
 
-function getX(x, beamSize, canvasWidth,) {
+function getX(x, beamSize, canvasWidth) {
   const scale = (canvasWidth - 2 * PADDING) / beamSize.length;
   return (x - beamSize.x) * scale + PADDING;
 }
